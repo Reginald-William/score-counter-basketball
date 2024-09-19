fd67efdd2d7e6fa71e2f52d18f42a42d1e73f2e2
Reginald-William <reginaldwilliam004@gmail.com>
2024-09-19 22:59:14 +0530

    Add Reset button
    On double click the scores of both the sides will be reset to zero.

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-let homeScore = document.getElementById("home-counter")
-let guestScore = document.getElementById("guest-counter")
+let homeScore = document.getElementById("home-counter");
+let guestScore = document.getElementById("guest-counter");
+let resetBtn = document.getElementById("btn-reset");
 
 function add1Home() {
-    homeScore.textContent = Number(homeScore.textContent) + 1
+  homeScore.textContent = Number(homeScore.textContent) + 1;
 }
 
 function add2Home() {
-    homeScore.textContent = Number(homeScore.textContent) + 2
+  homeScore.textContent = Number(homeScore.textContent) + 2;
 }
 
 function add3Home() {
-    homeScore.textContent = Number(homeScore.textContent) + 3
+  homeScore.textContent = Number(homeScore.textContent) + 3;
 }
 
-
 function add1Guest() {
-    guestScore.textContent = Number(guestScore.textContent) + 1
+  guestScore.textContent = Number(guestScore.textContent) + 1;
 }
 
 function add2Guest() {
-    guestScore.textContent = Number(guestScore.textContent) + 2
+  guestScore.textContent = Number(guestScore.textContent) + 2;
 }
 
 function add3Guest() {
-    guestScore.textContent = Number(guestScore.textContent) + 3
+  guestScore.textContent = Number(guestScore.textContent) + 3;
+}
+
+function scoreReset() {
+  homeScore.textContent = 0;
+  guestScore.textContent = 0;
 }
