983e7fefa9bdb40e315db9d9903f0df0d35c1eb2
Reginald-William <reginaldwilliam004@gmail.com>
2024-09-19 21:11:32 +0530

    Commit initial files

---
+++ index.js
@@ -0,0 +1,27 @@
+let homeScore = document.getElementById("home-counter")
+let guestScore = document.getElementById("guest-counter")
+
+function add1Home() {
+    homeScore.textContent = Number(homeScore.textContent) + 1
+}
+
+function add2Home() {
+    homeScore.textContent = Number(homeScore.textContent) + 2
+}
+
+function add3Home() {
+    homeScore.textContent = Number(homeScore.textContent) + 3
+}
+
+
+function add1Guest() {
+    guestScore.textContent = Number(guestScore.textContent) + 1
+}
+
+function add2Guest() {
+    guestScore.textContent = Number(guestScore.textContent) + 2
+}
+
+function add3Guest() {
+    guestScore.textContent = Number(guestScore.textContent) + 3
+}
